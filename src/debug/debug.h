f3fba82c9dd45f289004939aa0ff2c009df5f711
fgken <fg.bell.tree.333@gmail.com>
2015-02-27 19:03:03 +0900

    Implement bare metal printd by serial port on x86_64

--- src/debug/debug.h
+++ src/debug/debug.h
@@ -9,4 +9,15 @@ void printd(const char *buf)
 	while(buf[len++]);
 	
 	__syscall(SYS_write, 1 /*stdout*/, buf, len);
-} 
+}
+
+//x86_64 serial port output
+//void printd(const char *buf)
+//{
+//	unsigned long i;
+//
+//	for(i=0; buf[i] != 0x00; i++){
+//		asm volatile("outb %b0, %w1" : : "a"(buf[i]), "Nd"(0x3f8));
+//	}
+//} 
+
